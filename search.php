47def2de0f5d8f3c4556b12ce682ad27ec931a59
Andrew Wern Sern Er <s3259939@yallara.cs.rmit.edu.au>
2012-08-15 14:40:47 +1000

    assign1

--- search.php
+++ search.php
@@ -7,58 +7,155 @@
   <title>Explore Wines in a Region</title>
 </head>
 <body bgcolor="white">
-  <form action="query_results.php" method="GET">
-  
-  
-  	<br>
-  	Wine Name:
-  	<input type="text" name ="wineName" value="All">
-  
-  	<br>
-  	<br>
-  	
-  	Winery Name:
-  	<input type = "text" name ="wineryName value="All">
-  	
-  	<br>
-  	<br>
-  	
-  	Region Name:
-  	
-  	
-  	<?php
-	
-	//require_once('db.php');
-	//if (!$dbconn = mysql_connect(DB_HOST, DB_USER, DB_PW)) {
-	//echo 'Could not connect to mysql on ' . DB_HOST . "\n";
-	//exit; }
 
-	//echo 'Connected to mysql on ' . DB_HOST . "\n";
-	//if (!mysql_select_db(DB_NAME, $dbconn)) {
-	//echo 'Could not use database ' . DB_NAME . "\n";
-	//echo mysql_error() . "\n";
-	//exit;
-	//}
-	//echo 'Conndected to database ' . DB_NAME . "\n";
-
-   $options = '';
-   $filter =  mysql_query("select region_name from region") or die(mysql_error());
-   
-   while(($row = mysql_fetch_array($filter))!=false)
-   {
-      $options .="<option>" . $row['region_name'] . "</option>";
-        }
-               
-        $regionName="<form id ='regionName' name= 'regionName' method='get'>
-        <select name ='regionName'> ". $option ." </select>
-		</form>";
- 
-   echo "$regionName";
-               
+	<?php
+	require_once('connect.php');
 	?>
+	
+	
+	<form action="query_results.php" method="GET">
+	<table border ="1" action=" " method ="GET">
+  	<tr>
+		<td>
+			Wine Name:
+		</td>
+		<td>
+			<input type="text" name ="wineName" value="All">
+		</td>
+	</tr>
+	
+	<tr>
+		<td>
+			Winery Name:
+		</td>
+		
+		<td>
+		<input type = "text" name ="wineryName" value="All">
+		</td>
+	</tr>
 
+	<tr>
+		<td>
+			Region Name:
+		</td>
+		<td>
+			<?php
+
+			$options = '';
+			$filter =  mysql_query("select region_name from region") or die(mysql_error());
+   
+			while(($row = mysql_fetch_array($filter))!=false)
+			{
+				$options .="<option>" . $row['region_name'] ."</option>";
+			}
+			
+			$regionName="<form id ='regionName' name= 'regionName' method='get'>
+			
+			<select name ='regionName'> ". $options ." </select>
+			</form>";
+ 
+			echo "$regionName";
+               
+			?>
+		</td>
+	</tr>
+  
+	<tr>
+		<td>
+			Grape Variety:
+		</td>
+		<td>
+
+			<?php
+				$options = '';
+				$filter =  mysql_query("select variety from grape_variety") or die(mysql_error());
+   
+				while(($row = mysql_fetch_array($filter))!=false)
+				{
+					$options .="<option>" . $row['variety'] . "</option>";
+				}
+    
+				$variety="<form id ='variety' name= 'variety' method='get'>
+				<select name ='variety'> ". $options ." </select>
+				</form>";
+ 
+				echo "$variety";
+             ?>
+		</td>
+	</tr>
+	<tr>
+		<td>
+			Range Years:
+		</td>
+		<td>
+				From:
+				<?php
+				$options = '';
+				$filter =  mysql_query("select distinct year from wine order by year asc") or die(mysql_error());
+   
+				while(($row = mysql_fetch_array($filter))!=false)
+				{
+					
+					$options .="<option>" . $row['year'] . "</option>";
+				}
+				
+				echo $row['year'];
+				
+				$year="<form id ='year' name= 'year' method='get'>
+				<select name ='year'> ". $options ." </select>
+				</form>";
+				
+				echo "$year";
+				
+
+				?>
+				
+				To:
+				<?php
+				$options = '';
+				$filter =  mysql_query("select distinct year from wine order by year asc") or die(mysql_error());
+   
+				while(($row = mysql_fetch_array($filter))!=false)
+				{
+					
+					$options .="<option>" . $row['year'] . "</option>";
+				}
+				
+				echo $row['year'];
+				
+				$year="<form id ='year' name= 'year' method='get'>
+				<select name ='year'> ". $options ." </select>
+				</form>";
+				
+				echo "$year";
+				
+
+				?>
+				
+		</td>
+	</tr>
+	<tr>
+		<td>
+		Minimum Stock:
+		</td>
+		<td>
+		<input type="text" name ="minStock" value="All">
+		</td>
+	</tr>
+	<tr>
+		<td>
+		Minimum Orders:
+		</td>
+		<td>
+		<input type="text" name ="minOrders" value="All">
+		</td>
+	</tr>
+	<tr>
+		<td>
+		<input type='submit' name = 'submit' value ='SEARCH' />
+		</td>	
     
   </form>
-  <br>
+
 </body>
 </html>
