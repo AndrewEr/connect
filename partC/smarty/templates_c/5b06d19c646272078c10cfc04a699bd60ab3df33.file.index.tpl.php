cec1149ab2b891ddb4aa609fb03ab685c98b82d0
Andrew Wern Sern Er <s3259939@yallara.cs.rmit.edu.au>
2012-08-21 19:31:10 +1000

    added part c and instal smarty

---
+++ partC/smarty/templates_c/5b06d19c646272078c10cfc04a699bd60ab3df33.file.index.tpl.php
@@ -0,0 +1,22 @@
+<?php /* Smarty version Smarty-3.1.11, created on 2012-08-21 19:21:02
+         compiled from "smarty/templates/index.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:1015654413503352fea17e48-61217873%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '5b06d19c646272078c10cfc04a699bd60ab3df33' => 
+    array (
+      0 => 'smarty/templates/index.tpl',
+      1 => 1345540362,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '1015654413503352fea17e48-61217873',
+  'function' => 
+  array (
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.11',
+  'unifunc' => 'content_503352feb44932_03320653',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_503352feb44932_03320653')) {function content_503352feb44932_03320653($_smarty_tpl) {?>Hello<?php }} ?>
